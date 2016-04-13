d110dd5ce8dc9d3f3388716dfb537ea38d8584e0
Nikola Klipa <klipa.nikola@gmail.com>
2016-04-13 13:14:42 +0200

    Sending async requests and getting parameters in stream

--- src/AppBundle/Controller/DefaultController.php
+++ src/AppBundle/Controller/DefaultController.php
@@ -7,6 +7,11 @@ use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Symfony\Component\HttpFoundation\Request;
 
 use GuzzleHttp\Client;
+use GuzzleHttp\Pool;
+use GuzzleHttp\Psr7\Request as GuzzleRequest;
+
+use Symfony\Component\HttpFoundation\StreamedResponse;
+
 
 class DefaultController extends Controller
 {
@@ -15,13 +20,49 @@ class DefaultController extends Controller
      */
     public function indexAction(Request $request)
     {
-    	$client = $this->get('guzzle.client.api_crm');
-        $request = new \GuzzleHttp\Psr7\Request('GET', 'http://tivat.sistem48.me');
-		$promise = $client->sendAsync($request)->then(function ($response) {
-    		echo 'I completed! '.$response->getStatusCode();
-		});
-		// $promise->wait();
-        
         return $this->render('default/index.html.twig');
-    }
+	}
+
+	/**
+     * @Route("/check-emails", name="checkEmails")
+     */
+	public function checkEmailsAction(Request $request){
+		$m = $request->request->get('emails', 'default value if bar does not exist');
+		$emails = explode(",", $m);
+		$count = count($emails);
+
+		$response = new StreamedResponse();
+		$response->headers->set('Content-Type', 'text/event-stream');
+
+		$response->setCallback(function () use ($emails, $count) {
+			$client = $this->get('guzzle.client.api_crm');
+			
+			$requests = function ($total, $emails) {
+			    for ($i = 0; $i < $total; $i++) {
+			    	$uri = $emails[$i];
+			        yield new GuzzleRequest('GET', $uri);
+			    }
+			};
+
+			$pool = new Pool($client, $requests($count, $emails), [
+			    'concurrency' => 1000,
+			    'fulfilled' => function ($response, $index) {
+			        echo 'data: Index: '.$index.PHP_EOL;
+			        echo 'data: Status code: '.$response->getStatusCode().PHP_EOL;
+			        // echo 'data: Content length: '.$response->getHeader('Content-Length')[0].PHP_EOL;
+			        echo 'data: Content length: '.$response->getBody()->getSize().PHP_EOL;
+			        flush();
+			    },
+			    'rejected' => function ($reason, $index) {
+			    	echo 'data: Reason: '.$reason.PHP_EOL;
+			        flush();
+			    },
+			]);
+
+			$promise = $pool->promise();
+
+			$promise->wait();
+		});
+		return $response;
+	}
 }
