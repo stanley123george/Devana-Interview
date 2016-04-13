c86e103a07852c98a4b8b39a021f0553d80f5688
Nikola Klipa <klipa.nikola@gmail.com>
2016-04-14 00:03:47 +0200

    Sending as much as you want lists to backend and showing them in frontend
    Redirection is still missing

--- src/AppBundle/Controller/DefaultController.php
+++ src/AppBundle/Controller/DefaultController.php
@@ -24,45 +24,44 @@ class DefaultController extends Controller
 	}
 
 	/**
-     * @Route("/check-emails", name="checkEmails")
+     * @Route("/check-urls", name="checkUrls")
      */
-	public function checkEmailsAction(Request $request){
-		$m = $request->request->get('emails', 'default value if bar does not exist');
-		$emails = explode(",", $m);
-		$count = count($emails);
+	public function checkUrlsAction(Request $request){
+		$urlsString = $request->request->get('urls', array("http://www.google.com"));
+		$parent = $request->request->get('parent', 0);
+		$urls = explode(",", $urlsString);
+		$count = count($urls);  //obrada requesta
 
 		$response = new StreamedResponse();
 		$response->headers->set('Content-Type', 'text/event-stream');
-
-		$response->setCallback(function () use ($emails, $count) {
+		$response->setCallback(function () use ($urls, $count, $parent) {
 			$client = $this->get('guzzle.client.api_crm');
 			
-			$requests = function ($total, $emails) {
+			$requests = function ($total, $urls) {  //pool request funkcija
 			    for ($i = 0; $i < $total; $i++) {
-			    	$uri = $emails[$i];
+			    	$uri = $urls[$i];
 			        yield new GuzzleRequest('GET', $uri);
 			    }
 			};
 
-			$pool = new Pool($client, $requests($count, $emails), [
+			$pool = new Pool($client, $requests($count, $urls), [
 			    'concurrency' => 1000,
-			    'fulfilled' => function ($response, $index) {
-			        echo 'data: Index: '.$index.PHP_EOL;
-			        echo 'data: Status code: '.$response->getStatusCode().PHP_EOL;
-			        // echo 'data: Content length: '.$response->getHeader('Content-Length')[0].PHP_EOL;
-			        echo 'data: Content length: '.$response->getBody()->getSize().PHP_EOL;
+			    'fulfilled' => function ($response, $index) use ($urls, $parent){ //dobijen $response za url sa $index
+			        $array = array("url" => $urls[$index], "status"=>$response->getStatusCode(), "length"=>$response->getBody()->getSize(), "parent"=>$parent, "index"=>$index);
+			        echo json_encode($array);
 			        flush();
 			    },
-			    'rejected' => function ($reason, $index) {
+			    'rejected' => function ($reason, $index) { 
+			    	echo 'data: Index: '.$index.PHP_EOL;
 			    	echo 'data: Reason: '.$reason.PHP_EOL;
 			        flush();
 			    },
 			]);
 
-			$promise = $pool->promise();
-
+			$promise = $pool->promise();  //trazi da svi urlovi iz pola dobiju odgovor
 			$promise->wait();
 		});
+
 		return $response;
 	}
 }
